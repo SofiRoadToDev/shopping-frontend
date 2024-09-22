e0dc7fe2ed18f26e6440d0d596585cfdd073e8e6
SofiRoadToDev <sofi.workaffairs@gmail.com>
2024-09-22 00:11:49 -0300

    list and grid with 2 cols,sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TableListComponent } from './components/table-list/table-list.component';
+import { CardComponent } from './components/card/card.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'productos', component: CardComponent },
+  { path: '', component: TableListComponent },
+  { path: 'checkout', component: CheckoutComponent },
+];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBars, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FontAwesomeModule, NgClass],
+  imports: [FontAwesomeModule, NgClass, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
